082bfb9c547338cc061861d7c5ac344d06679fd3
kimocoder <christianbremvaag@gmail.com>
2019-02-11 00:15:55 +0100

    Added 1 VID/PID and removed 1 execute permission

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -150,7 +150,8 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	{USB_DEVICE(USB_VENDER_ID_REALTEK, 0x0179), .driver_info = RTL8188E}, /* 8188ETV */
 	/*=== Customer ID ===*/
 	/****** 8188EUS ********/
-	{USB_DEVICE(0x07B8, 0x8179), .driver_info = RTL8188E}, /* Abocom - Abocom */
+	{USB_DEVICE(0x07B8, 0x8179), .driver_info = RTL8188E}, /* TP-Link */
+	{USB_DEVICE(0x0bda, 0x8179), .driver_info = RTL8188E}, /* Abocom - Abocom */
 #endif
 
 #ifdef CONFIG_RTL8812A
