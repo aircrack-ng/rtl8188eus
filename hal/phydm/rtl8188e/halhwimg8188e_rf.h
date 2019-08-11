db0c7c7a1b2d83067fb9e588acfeedf896df952f
kimocoder <christian@aircrack-ng.org>
2019-08-11 22:15:32 +0000

    Upload new v5.7.6 base

---
+++ hal/phydm/rtl8188e/halhwimg8188e_rf.h
@@ -0,0 +1,118 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2017 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ *****************************************************************************/
+
+/*Image2HeaderVersion: 3.5.2*/
+#if (RTL8188E_SUPPORT == 1)
+#ifndef __INC_MP_RF_HW_IMG_8188E_H
+#define __INC_MP_RF_HW_IMG_8188E_H
+
+
+/******************************************************************************
+*                           radioa.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_radioa( /* tc: Test Chip, mp: mp Chip*/
+				    struct dm_struct *dm);
+u32 odm_get_version_mp_8188e_radioa(void);
+
+/******************************************************************************
+*                           txpowertrack_ap.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpowertrack_ap( /* tc: Test Chip, mp: mp Chip*/
+					     struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpowertrack_ap(void);
+
+/******************************************************************************
+*                           txpowertrack_pcie.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpowertrack_pcie( /* tc: Test Chip, mp: mp Chip*/
+					       struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpowertrack_pcie(void);
+
+/******************************************************************************
+*                           txpowertrack_pcie_icut.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpowertrack_pcie_icut(
+						    /* tc: Test Chip, mp: mp Chip*/
+						    struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpowertrack_pcie_icut(void);
+
+/******************************************************************************
+*                           txpowertrack_sdio.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpowertrack_sdio( /* tc: Test Chip, mp: mp Chip*/
+					       struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpowertrack_sdio(void);
+
+/******************************************************************************
+*                           txpowertrack_sdio_icut.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpowertrack_sdio_icut(
+						    /* tc: Test Chip, mp: mp Chip*/
+						    struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpowertrack_sdio_icut(void);
+
+/******************************************************************************
+*                           txpowertrack_usb.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpowertrack_usb( /* tc: Test Chip, mp: mp Chip*/
+					      struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpowertrack_usb(void);
+
+/******************************************************************************
+*                           txpowertrack_usb_icut.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpowertrack_usb_icut(
+						   /* tc: Test Chip, mp: mp Chip*/
+						   struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpowertrack_usb_icut(void);
+
+/******************************************************************************
+*                           txpwr_lmt.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpwr_lmt( /* tc: Test Chip, mp: mp Chip*/
+				       struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpwr_lmt(void);
+
+/******************************************************************************
+*                           txpwr_lmt_88ee_m2_for_msi.TXT
+******************************************************************************/
+
+void
+odm_read_and_config_mp_8188e_txpwr_lmt_88ee_m2_for_msi(
+						       /* tc: Test Chip, mp: mp Chip*/
+						       struct dm_struct *dm);
+u32	odm_get_version_mp_8188e_txpwr_lmt_88ee_m2_for_msi(void);
+
+#endif
+#endif /* end of HWIMG_SUPPORT*/
+
---
+++ hal/phydm/rtl8188e/phydm_rtl8188e.c
@@ -0,0 +1,58 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2017 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ *****************************************************************************/
+
+/* ************************************************************
+ * include files
+ * ************************************************************ */
+
+#include "mp_precomp.h"
+
+#include "../phydm_precomp.h"
+
+#if (RTL8188E_SUPPORT == 1)
+
+s8 phydm_cck_rssi_8188e(struct dm_struct *dm, u16 lna_idx, u8 vga_idx)
+{
+	s8 rx_pwr_all = 0;
+	s8 lna_gain = 0;
+	/*only use lna0/1/2/3/7*/
+	s8 lna_gain_table_0[8] = {17, -1, -13, -29, -32, -35, -38, -41};
+	/*only use lna3 /7*/
+	s8 lna_gain_table_1[8] = {29, 20, 12, 3, -6, -15, -24, -33};
+	/*only use lna1/3/5/7*/
+	s8 lna_gain_table_2[8] = {17, -1, -13, -17, -32, -43, -38, -47};
+	
+	if (dm->cut_version >= ODM_CUT_I) { /*SMIC*/
+		if (dm->ext_lna == 0x1) {
+			switch (dm->type_glna) {
+				case 0x2:	/*eLNA 14dB*/
+					lna_gain = lna_gain_table_2[lna_idx];
+					break;
+				default:
+					lna_gain = lna_gain_table_0[lna_idx];
+					break;
+			}
+		} else {
+			lna_gain = lna_gain_table_0[lna_idx];
+		}		
+	} else { /*TSMC*/
+		lna_gain = lna_gain_table_1[lna_idx];
+	}
+	
+	rx_pwr_all = lna_gain - (2 * vga_idx);
+
+	return rx_pwr_all;
+}
+#endif
