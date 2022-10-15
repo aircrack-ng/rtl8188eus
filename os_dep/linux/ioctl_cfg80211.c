6899e318eff7906e93003feb3781c48d281e72d7
KanuX-14 <kanux.dev@gmail.com>
2022-10-15 07:36:48 -0300

    'cfg80211_ch_switch_notify' function added to kernel 5.19.1+ Reference: https://github.com/torvalds/linux/blob/master/include/net/cfg80211.h#L8276

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -419,6 +419,8 @@ u8 rtw_cfg80211_ch_switch_notify(_adapter *adapter, u8 ch, u8 bw, u8 offset, u8 
 		
 	#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 2))
 	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef);
+	#else
+	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef, 0);
 	#endif
 #else
 	int freq = rtw_ch2freq(ch);
