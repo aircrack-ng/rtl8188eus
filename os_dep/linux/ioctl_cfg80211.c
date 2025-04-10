4aa31c0f7ec0f8dc805853d0e82be6402139efb7
gluker <gluker@gmail.com>
2025-04-10 04:13:56 +0300

    Build support for kernel 6.14

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -4018,6 +4018,10 @@ static int cfg80211_rtw_get_txpower(struct wiphy *wiphy,
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0))
 	struct wireless_dev *wdev,
 #endif
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0))
+        unsigned int link_id,
+#endif
+
 	int *dbm)
 {
 	_adapter *padapter = wiphy_to_adapter(wiphy);
